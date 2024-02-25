ac2ab78af88b57862d07f56935db94758fd4af96
shivangiag08 <shivangiag08@gmail.com>
2024-02-25 09:00:39 +0530

    ""

--- finetuning_llama.py
+++ finetuning_llama.py
@@ -3,10 +3,10 @@ import replicate
 training = replicate.trainings.create(
   version="meta/llama-2-7b:73001d654114dad81ec65da3b834e2f691af1e1526453189b7bf36fb3f32d0f9",
   input={
-    "train_data": "https://gist.githubusercontent.com/nateraw/055c55b000e4c37d43ce8eb142ccc0a2/raw/d13853512fc83e8c656a3e8b6e1270dd3c398e77/samsum.jsonl",
+    "train_data": "https://github.com/shivangiag08/dumbl/blob/main/workout_data.json",
     "num_train_epochs": 3,
   },
-  destination="<your-username>/llama2-summarizer"
+  destination="shivangiag08/llama2-gymbro"
 )
 
 print(training)
