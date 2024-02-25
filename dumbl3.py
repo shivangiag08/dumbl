c2a8dff732240b921f88b93afad79ee7f8db5fc8
shivangiag08 <shivangiag08@gmail.com>
2024-02-25 05:53:20 +0530

    ""

--- dumbl3.py
+++ dumbl3.py
@@ -11,7 +11,14 @@ data = data.drop(columns=["images","instructions","mechanic","id"])
 
 st.title('💪🏼 Workout Recommender')
 if 'REPLICATE_API_TOKEN' in st.secrets:
+    key = 1
     replicate_api = st.secrets['REPLICATE_API_TOKEN']
+else:
+    replicate_api = st.text_input('Enter Replicate API token:', type='password')
+    if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+        st.warning('Please enter your credentials!', icon='⚠️')
+    else:
+        st.success('Proceed to entering your prompt message!', icon='👉')
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
 # mapping the equipment and level to numerical values
