bae627121b2580f8ff89af73751bf37baadb8a2a
shivangiag08 <shivangiag08@gmail.com>
2024-02-25 05:42:12 +0530

    ""

--- dumbl3.py
+++ dumbl3.py
@@ -59,7 +59,7 @@ relevant_exercises = data[(data["level"] == level_mapping[level]) & (data["equip
 
 # function to generate workout suggestion
 def generate_workout_suggestion(prompt_input, relevant_exercises):
-    llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+    llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
 
 
     string_dialogue = f"You are a fitness assistant. Recommend the ideal workout based on the following:\n"
