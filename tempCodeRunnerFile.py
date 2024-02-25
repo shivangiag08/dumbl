950be2b86efd0e1d2f02870c21207375a6c0a427
shivangiag08 <shivangiag08@gmail.com>
2024-02-25 09:00:51 +0530

    ''

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,4 @@
 import replicate
-import os
-
-# Ensure you have set an environment variable named 'REPLICATE_API_TOKEN' with your token
-api_token = os.getenv('REPLICATE_API_TOKEN')
-if not api_token:
-    raise ValueError("Please set the REPLICATE_API_TOKEN environment variable.")
-
 
 training = replicate.trainings.create(
   version="meta/llama-2-7b:73001d654114dad81ec65da3b834e2f691af1e1526453189b7bf36fb3f32d0f9",
